425e39390285388ceb79824d95be609316000a04
Shengwen Yu <yshengwen@vmware.com>
2024-07-12 10:59:34 +0800

    fix: migrate corporate harbor to "registry.goharbor.io"
    Signed-off-by: Shengwen Yu <yshengwen@vmware.com>

--- src/io/goharbor/CaseSettings.groovy
+++ src/io/goharbor/CaseSettings.groovy
@@ -5,7 +5,7 @@ public class CaseSettings implements Serializable{
     public String repository // the repository of test cases
     public String branch // the branch/tag of the test case repository
     public String cases // the test cases[database/ldap/oidc/uaa/trivy/notary/chartmuseum] which run against the Harbor instance. Split multiple items by comma
-    public String imageRegistry // the registry where stores the test images, default: harbor-repo.vmware.com
+    public String imageRegistry // the registry where stores the test images, default: registry.goharbor.io
     public String imageRepositoryPrefix // the prefix of the repository where stores the test images, default: harbor-ci
     public String dockerHubUsername
     public String dockerHubPassword
